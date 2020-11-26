08ffd85e9008cc45d9ccec19951d85c015a93255
Tracyfoster <tracyfoster99@gmail.com>
2020-11-26 05:22:47 +0100

    Setup Auth0

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,10 @@
 class ApplicationController < ActionController::API
+
+  private
+
+  def authorize_request
+    AuthorizationService.new(request.headers).authenticate_request!
+  rescue JWT::VerificationError, JWT::DecodeError
+    render json: { errors: ['Not Authenticated'] }, status: :unauthorized
+  end
 end
