18c474c071adf22b6eca42fdb794a59797e71e22
Tracyfoster <tracyfoster99@gmail.com>
2020-11-27 08:08:22 +0100

    Remove initial auth0 setup

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,2 @@
 class ApplicationController < ActionController::API
-  private
-
-  def authorize_request
-    AuthorizationService.new(request.headers).authenticate_request!
-  rescue JWT::VerificationError, JWT::DecodeError
-    render json: { errors: ["Not Authenticated"] }, status: :unauthorized
-  end
 end
