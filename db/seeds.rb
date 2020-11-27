fc51694b058c4a3402da3763164c55a7df9c4242
Tracyfoster <tracyfoster99@gmail.com>
2020-11-27 22:01:24 +0100

    Create seed file for courses and learning module

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,35 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+INDUSTRY = %w[Finance Insurane Real-Estate].freeze
+PROF_TYPES = %w[beginner advanced expert].freeze
+
+def create_learning_modules
+  FactoryBot.create_list(:learning_module, 5,
+                         industry: INDUSTRY.shuffle.last,
+                         professional_level: PROF_TYPES.shuffle.last)
+end
+
+def create_basic_course(learning_module)
+  FactoryBot.create(:course,
+                         learning_module: learning_module,
+                         subscription_level: "basic")
+end
+def create_premium_course(learning_module)
+  FactoryBot.create(:course,
+                         learning_module: learning_module,
+                         subscription_level: :premium)
+end
+def create_professional_course(learning_module)
+  FactoryBot.create(:course,
+                         learning_module: learning_module,
+                         subscription_level: 2)
+end
+
+
+learning_modules = create_learning_modules
+learning_modules.each do |learning_module|
+  create_basic_course(learning_module)
+  create_premium_course(learning_module)
+  create_professional_course(learning_module)
+end
